b885bda63aacff10b54701d25756c94719950515
KanayoAlexandria <kanayoalexandria@gmail.com>
2022-04-12 11:12:12 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -14,20 +14,3 @@ int _abs(int n)
 	else
 		return (n * -1);
 }
-
-TASK 7
-#include "main.h"
-#include "6-abs.c"
-/**
- * print_last_digit - main function
- *
- * @n: integer to get last digit of
- * Return: last digit of n
- *
- */
-
-int print_last_digit(int n)
-{
-	_putchar('0' + _abs(n % 10));
-	return (_abs(n % 10));
-}
